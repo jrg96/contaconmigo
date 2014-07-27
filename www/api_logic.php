cc85c0b455506f9ef5dad80840982a0b65ddb90c
GhostAlgorithm <gomezlopez.jorge@yahoo.es>
2014-07-27 07:15:29 +0200

    Inicio del proyecto

---
+++ www/api_logic.php
@@ -0,0 +1,194 @@
+<?php 
+	header('Content-Type: text/html; charset=utf-8');
+
+	$GLOBALS['dbh'] = null;
+	try {
+    	$GLOBALS['dbh'] = new PDO("mysql:host=localhost;dbname=cuenta_conmigo", "root", "codersv");
+    }catch(PDOException $e){
+    	$GLOBALS['dbh'] =$e->getMessage();
+    }
+
+	function getData($path){
+		$myCurl=curl_init();
+		curl_setopt($myCurl, CURLOPT_URL, "http://api.gobiernoabierto.gob.sv/".$path);
+		curl_setopt($myCurl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($myCurl, CURLOPT_HTTPHEADER, array(
+		  'Authorization:  Token  token="36bc11762f97f155729497f7099d76c4"'
+		));
+		$response = curl_exec($myCurl);
+		$xml = new SimpleXMLElement($response);
+
+		return $xml;
+	}
+
+	function getStates(){
+		$states=getData("states/index.xml");
+
+		$stateSelect="<select id='select_dept' class='form-control col-xs-12 col-md-6'>";
+		foreach ($states as $single) {
+			$stateSelect.="<option value=".$single->id.">".$single->name."</option>";
+		}
+		$stateSelect.="</select>";
+
+		return $stateSelect;
+	}
+
+	function getCityByState($state){
+		$cities=getData("cities/index.xml?&q[state_id_eq]=".$state."&per_page=100");
+
+		$citieSelect="<select>";
+		foreach ($cities as $single) {
+			$citieSelect.="<option value=".$single->id.">".$single->name."</option>";
+		}
+		$citieSelect.="</select>";
+
+		return $citieSelect;
+	}
+
+	function typesOrg(){
+		$types=getData("civil_organization_types/index.xml");
+
+		$typeSelect="<select>";
+		foreach ($types as $single) {
+			$typeSelect.="<option value=".$single->id.">".$single->name."</option>";
+		}
+		$typeSelect.="</select>";
+
+		return $typeSelect;
+	}
+
+	function orgByType($type){
+		$organization=array();
+		$page=0;
+		$index=0;
+
+		do {
+			$page++;
+			$index=0;
+			$orgs=getData("civil_organizations/index.xml?q[civil_organization_type_id_eq]=".$type."&per_page=100&page=".$page);
+
+			foreach ($orgs as $org) {
+				$index++;
+				$actIndex=($page*100)-100+$index;
+				$organization[$actIndex]=array();
+				$organization[$actIndex][0]=$org->id;
+				$organization[$actIndex][1]=$org->name;
+				$organization[$actIndex][2]=$org->address;
+				$organization[$actIndex][3]=$org->phone;
+				$organization[$actIndex][4]=$org->manager;
+				$organization[$actIndex][5]=$org->slug;
+				$organization[$actIndex][6]=$org->email;
+				$organization[$actIndex][7]=$org->civil_organization_type_id;
+			}
+
+		} while($index==100);
+
+		return $organization;
+	}
+
+	function orgByTypeAndPage($type,$page){
+		$organization=array();
+		$index=0;
+
+		$orgs=getData("civil_organizations/index.xml?q[civil_organization_type_id_eq]=".$type."&per_page=100&page=".$page);
+
+		foreach ($orgs as $org) {
+			$index++;
+			$organization[$index]=array();
+			$organization[$index][0]=$org->id;
+			$organization[$index][1]=$org->name;
+			$organization[$index][2]=$org->address;
+			$organization[$index][3]=$org->phone;
+			$organization[$index][4]=$org->manager;
+			$organization[$index][5]=$org->slug;
+			$organization[$index][6]=$org->email;
+			$organization[$index][7]=$org->civil_organization_type_id;
+		}
+
+		return $organization;
+	}
+
+	function orgDataById($id){
+		$orgData=getData("civil_organizations/index.xml?&q[id_eq]=".$id);
+
+		foreach ($orgData as $single) {
+			$typeSelect.="<option value=".$single->id.">".$single->name."</option>";
+		}
+		
+		return $typeSelect;
+	}
+
+	function orgDataInfoByStateFromDB($state){
+		$organizations=array();
+		$index=0;
+
+		$sql = "SELECT * FROM user_info WHERE estate='".$state."'";
+	    
+	    foreach ($GLOBALS['dbh']->query($sql) as $row){
+	    	$index++;
+			$organizations[$index]=array();
+			$organizations[$index][0]=$row['id'];
+			$organizations[$index][1]=$row['iduser'];
+			$organizations[$index][2]=$row['latitude'];
+			$organizations[$index][3]=$row['longitude'];
+			$organizations[$index][4]=$row['area'];
+			$organizations[$index][5]=$row['cel'];
+			$organizations[$index][6]=$row['city'];
+			$organizations[$index][7]=$row['estate'];
+
+			$orgData=getData("civil_organizations/index.xml?&q[id_eq]=".$organizations[$index][0]);
+
+			foreach ($orgData as $single) {
+				$organizations[$index][8]=$single->address;
+				$organizations[$index][9]=$single->civil_organization_type_id;
+				$organizations[$index][10]=$single->email;
+				$organizations[$index][11]=$single->manager;
+				$organizations[$index][12]=$single->name;
+				$organizations[$index][13]=$single->phone;
+				$organizations[$index][14]=$single->slug;
+			}
+
+	    }
+
+	    return $organizations;
+	}
+
+
+	function refuges(){
+		$refuges=array();
+		$page=0;
+		$index=0;
+
+		do {
+			$page++;
+			$index=0;
+			$refuge=getData("refuges/index.xml?per_page=100&page=".$page);
+
+			foreach ($refuge as $ref) {
+				$index++;
+				$actIndex=($page*100)-100+$index;
+				$refuges[$actIndex]=array();
+				$refuges[$actIndex][0]=$ref->address;
+				$refuges[$actIndex][1]=$ref->city_id;
+				$refuges[$actIndex][2]=$ref->contact_person;
+				$refuges[$actIndex][3]=$ref->families_capacity;
+				$refuges[$actIndex][4]=$ref->has_bathroom;
+				$refuges[$actIndex][5]=$ref->has_communication;
+				$refuges[$actIndex][6]=$ref->has_electricity;
+				$refuges[$actIndex][7]=$ref->has_toilet;
+				$refuges[$actIndex][8]=$ref->has_water;
+				$refuges[$actIndex][9]=$ref->has_toilet;
+				$refuges[$actIndex][10]=$ref->id;
+				$refuges[$actIndex][11]=$ref->kind;
+				$refuges[$actIndex][12]=$ref->name;
+				$refuges[$actIndex][13]=$ref->people_capacity;
+				$refuges[$actIndex][14]=$ref->phone;
+				$refuges[$actIndex][15]=$ref->slug;	
+			}
+
+		} while($index==100);
+
+		return $refuges;
+	}
+
+?>
---
+++ www/busqueda.php
@@ -0,0 +1,63 @@
+<?php include 'api_logic.php';?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="common/css/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="common/css/main.css"/>
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map_canvas { height: 100% }
+    </style>
+    <script type="text/javascript"
+      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBYuhiS0SauIzg3yLxlsIlF4knSwrIW_cU&sensor=true">
+    </script>
+    <script type="text/javascript">
+		var map = null;
+		var markers = [];
+		var circles = [];
+		
+		function initialize() {
+			var mapOptions = {
+				center: new google.maps.LatLng(13.685449, -89.239938),
+				zoom: 18,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			map = new google.maps.Map(document.getElementById("map_canvas"),
+				mapOptions);
+		}
+    </script>
+	<script src="map_funcs.js"></script>
+	<script src="test.js"></script>
+  </head>
+  <body onload="initialize()">
+	<!--Menu -->
+	<section class="main-menu">
+		<center><img class="logo" class="pull-left" src="common/img/logo.png" title="ContaConmigo"></center>
+	</section>
+	<!--search -->
+		<div class="col-xs-12  col-md-12 clearfix searching">
+			<br>
+				<div class="col-md-2"></div>
+				<div class="col-xs-12 col-md-8">
+					<div class="col-xs-12 col-md-6">
+						<label>Departamento:</label>
+						<?php echo getStates(); ?>
+					</div>
+					<div class="col-xs-12 col-md-6">
+						<label>Tipo de organización:</label>
+							<select class="form-control">
+								<option>a</option>
+							</select>
+					</div>
+				</div>
+		</div>
+	<br>
+	<br>
+	<!--map -->
+    <div id="map_canvas" style="width:100%; height:80%"></div>
+  </body>
+</html>
