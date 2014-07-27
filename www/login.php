9ce581c1f33e6a8ab082ebd408a442c67acf2af0
GhostAlgorithm <gomezlopez.jorge@yahoo.es>
2014-07-27 22:50:13 +0200

    start of login

---
+++ www/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<title>ContáConmigo</title>
+	<!--Bootstrap / css / fonts-->
+	<meta charset="UTF-8"/>		
+	<link rel="stylesheet" type="text/css" href="common/css/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="common/css/font-awesome.css"/>
+	<link rel="stylesheet" type="text/css" href="common/css/main1.css"/>
+	<link rel="stylesheet" type="text/css" href="common/css/buttons.css"/>
+
+	<!--Responsive-->
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+<body>
+	<form>
+		<center><img src="common/img/logo.png"></center>
+			<div class="panel panel-default">
+				<div class="panel-heading col-lg-4 col-lg-offset-4 col-md-5 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-7 col-xs-offset-3">
+						<div>
+						<h4>Usuario:</h4>
+						<input type="email" class="form-control" placeholder="abc@email.com" required/>
+						<h4>Contraseña:</h4>
+						<input type="password" class="form-control" placeholder="*****" required/>
+						<br>
+						<button class="btn btn-lg btn-success pull-right" type="submit" name="LogIn">Iniciar Sesión</button>
+					</div>		
+		</div>
+		</div>
+		</div>
+	</form>
+
+
+	<script src="common/js/bootstrap.js"></script>
+	<script src="common/js/jquery"></script>
+</body>
+</html>
